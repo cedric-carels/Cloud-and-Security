9281eb35db2e1e23c18da5a1a42a80411ef9b963
cedric-carels <61278009+cedric-carels@users.noreply.github.com>
2021-03-01 09:57:45 +0100

    Add files via upload

---
+++ Opdracht 1/ESP32-MQTT/setup.h
@@ -0,0 +1,12 @@
+#ifndef WIFI_H_INCLUDE
+#define WIFI_H_INCLUDE
+
+//gebruik van leerkracht op school WIFI
+#define ssid "AndroidAPjy"
+#define password "totototo"
+
+//gebruik van thuis WIFI
+//#define ssid "home wifi"
+//#define password "home wachtwoord"
+
+#endif
