1b6dbcaf6206d10bbc75ea96742b2052adf00cbd
cedric-carels <61278009+cedric-carels@users.noreply.github.com>
2021-03-21 14:54:39 +0100

    Add files via upload

---
+++ Assignment 1/ESP32-MQTT/setup.h
@@ -0,0 +1,12 @@
+#ifndef WIFI_H_INCLUDE
+#define WIFI_H_INCLUDE
+
+//gebruik van WIFI
+//#define ssid "AndroidAPjy"
+//#define password "totototo"
+
+//gebruik van hotspot
+#define ssid "NETGEAR09"
+#define password "sunnyrosebud462"
+
+#endif
